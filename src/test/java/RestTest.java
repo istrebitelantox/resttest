4db8b94551b3189e9e6e7ae77ddad33dc90809ba
istrebitelantox <geekpook@gmail.com>
2022-10-20 13:41:42 +0300

    complete RestTest

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pojos.UserRequest;
+import pojos.CreateUserResponse;
+import pojos.UserPojoFull;
+import utils.RestWrapper;
+import utils.UserGenerator;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestTest {
+    private static RestWrapper api;
+
+    @BeforeClass
+    public static void prepareClient()
+    {
+        api=RestWrapper.loginAs("eve.holt@reqres.in","cityslicka");
+    }
+
+    @Test
+    public void getUser()
+    {
+       assertThat(api.user.getUsers()).extracting(UserPojoFull::getEmail).contains("george.bluth@reqres.in");
+    }
+    @Test
+    public void createUser()
+    {
+        UserRequest rq= UserGenerator.getSimpleUser();
+        CreateUserResponse rs=api.user.createUser(rq);
+
+        assertThat(rs)
+                .isNotNull()
+                .extracting(CreateUserResponse::getName)
+                .isEqualTo(rq.getName());
+    }
+}
---
+++ src/test/java/utils/services/OrderSevice.java
@@ -0,0 +1,26 @@
+package utils.services;
+
+import io.restassured.http.Cookies;
+import pojos.UserPojoFull;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderSevice extends RestService {
+
+    @Override
+    protected String getBasePath() {
+        return "/orders";
+    }
+
+    public OrderSevice(Cookies cookies) {
+        super(cookies);
+    }
+
+    public List<UserPojoFull> getOrders(){
+        return given().spec(REQ_SPEC)
+                .get()
+                .jsonPath().getList("data", UserPojoFull.class);
+    }
+}
